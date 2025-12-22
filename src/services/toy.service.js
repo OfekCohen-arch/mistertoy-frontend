ec274af3f5a4600ea7647a3b860061f20ea46db3
Ofek Cohen <ofek074@gmail.com>
2025-12-22 15:41:58 +0200

    create toy.service.js

---
+++ src/services/toy.service.js
@@ -0,0 +1,106 @@
+import { httpService } from "./http.service.js";
+import { utilService } from "./util.service.js";
+
+const STORAGE_KEY = 'toy'
+export const toyService = {
+    query,
+    getById,
+    remove,
+    save,
+    getEmptyToy,
+    getDefaultFilter,
+    getLabels,
+    getLabelsStats,
+    getToysInStockStats,
+}
+async function query(filterBy = {}) {
+    return httpService.get(STORAGE_KEY, filterBy)
+}
+function getById(toyId) {
+    return httpService.get(`toy/${toyId}`)
+}
+
+async function remove(toyId) {
+    return httpService.delete(`toy/${toyId}`)
+}
+async function save(toy) {
+    var savedToy
+    if (toy._id) {
+        savedToy = await httpService.put(`toy/${toy._id}`, toy)
+
+    } else {
+        savedToy = await httpService.post('toy', toy)
+    }
+    return savedToy
+}
+function getEmptyToy() {
+  return {
+    name: "",
+    imgUrl: "https://robohash.org/123",
+    price: 0,
+    labels: [],
+    inStock: true,
+  };
+}
+
+function getDefaultFilter() {
+  return { name: "", inStock: "", labels: [] };
+}
+function  _createToys(){
+  const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+'Outdoor', 'Battery Powered']
+for (let i = 0; i < 10; i++) {
+  const toy = {
+    name: utilService.makeLorem(2),
+    imgUrl: "https://robohash.org/123",
+    price: utilService.getRandomIntInclusive(50,200),
+    labels: [labels[utilService.getRandomIntInclusive(0,labels.length-1)]],
+    inStock: (i % 2 === 0) ? true : false
+  }
+  save(toy)
+}
+}
+function getLabels(){
+  return ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+'Outdoor', 'Battery Powered']
+}
+function getLabelsStats(){
+  return storageService.query(STORAGE_KEY)
+  .then(toys=>{
+    const toysCountByLabelMap = _getToysCountByLabelMap(toys)
+    const data = Object.keys(toysCountByLabelMap).map(label=>toysCountByLabelMap[label])
+    return data
+  }
+  )
+}
+function getToysInStockStats(){
+  return storageService.query(STORAGE_KEY)
+  .then(toys=>{
+    const toysInStockByLabelMap = _getToysInStockByLabelMap(toys)
+    const data = Object.keys(toysInStockByLabelMap).map(label=>toysInStockByLabelMap[label])
+    return data
+  })
+}
+function _getToysCountByLabelMap(toys){
+  const labels = toyService.getLabels()
+  const labelsMap = {}
+  labels.forEach(label=> labelsMap[label] = 0)
+const toysCountByLabelMap = toys.reduce((map,toy)=>{
+ const toyLabels = toy.labels
+ toyLabels.forEach(label=>map[label]++)
+ return map
+},labelsMap)
+return toysCountByLabelMap
+}
+function _getToysInStockByLabelMap(toys){
+  const labels = toyService.getLabels()
+  const labelsMap = {}
+  labels.forEach(label=> labelsMap[label] = 0)
+const toysInStockByLabelMap = toys.reduce((map,toy)=>{
+ const toyLabels = toy.labels
+ if(toy.inStock) toyLabels.forEach(label=>map[label]++)
+ return map
+},labelsMap)
+return toysInStockByLabelMap
+}
+
